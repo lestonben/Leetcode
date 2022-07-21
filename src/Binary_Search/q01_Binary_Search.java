d0cd344761527c1f82386ee76e5df30be2ae5f7c
ben <lestonben@gmail.com>
2022-07-21 13:42:04 +0800

    Updated

--- src/Binary_Search/q01_Binary_Search.java
+++ src/Binary_Search/q01_Binary_Search.java
@@ -6,7 +6,7 @@ public class q01_Binary_Search {
         int start = 0, end = nums.length-1;
 
         while (start <= end) {
-            int mid = start + (end-start)/2;
+            int mid = (start+end)/2;
 
             if (nums[mid] == target) return mid;
             else if (nums[mid] > target) end = mid-1;
