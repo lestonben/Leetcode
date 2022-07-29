2cb7f42a1d0ac32ddb5f5b489acb4142ea1b98dc
lestonben <lestonben@gmail.com>
2022-07-29 14:53:42 +0800

    commit

--- src/Binary_Search/q05_Intersection_of_Two_Arrays.java
+++ src/Binary_Search/q05_Intersection_of_Two_Arrays.java
@@ -10,23 +10,16 @@ public class q05_Intersection_of_Two_Arrays {
 
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set = new HashSet<>();
-        Arrays.sort(nums2);
         for (int i: nums1) {
             if (binarySearch(nums2, i)) {
                 set.add(i);
             }
         }
-        int[] uniques = new int[set.size()];
-        int idx=0;
-        for (int i: set) {
-            uniques[idx++] = i;
-        }
-        return uniques;
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public boolean binarySearch(int[] nums, int target) {
         int low = 0, high = nums.length-1;
-
         while (low <= high) {
             int mid = (low+high)/2;
             if (nums[mid] == target) { return true; }
