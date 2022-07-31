b57e67dab782af98ab78c1af4831837f596c75d7
lestonben <lestonben@gmail.com>
2022-07-31 18:10:41 +0800

    commit

---
+++ src/Linked_List/q07_Copy_List_with_Rand_Number.java
@@ -0,0 +1,41 @@
+package Linked_List;
+
+/*
+    https://leetcode.com/problems/copy-list-with-random-pointer/
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class q07_Copy_List_with_Rand_Number {
+
+    public Node copyRandomList(Node head) {
+
+        Map<Node, Node> map = new HashMap<>();
+        Node temp = head;
+        while (temp != null) {
+            map.put(temp, new Node(temp.val));
+            temp = temp.next;
+        }
+
+        temp = head;
+        while (temp != null) {
+            map.get(temp).next = map.get(temp.next);
+            map.get(temp).random = map.get(temp.random);
+            temp = temp.next;
+        }
+        return map.get(head);
+    }
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
