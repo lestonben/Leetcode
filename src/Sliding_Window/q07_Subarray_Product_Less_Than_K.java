8a0d701a61bb6172643e03b5270b36d7159c0b9d
lestonben <lestonben@gmail.com>
2022-08-03 10:26:21 +0800

    commit

---
+++ src/Sliding_Window/q07_Subarray_Product_Less_Than_K.java
@@ -0,0 +1,36 @@
+package Sliding_Window;
+
+/*
+    https://leetcode.com/problems/subarray-product-less-than-k/
+ */
+
+public class q07_Subarray_Product_Less_Than_K {
+
+    public static void main(String[] args) {
+        System.out.println(numSubArrayProductLessThanK(new int[]{10,5,2,6}, 100));
+    }
+
+    public static int numSubArrayProductLessThanK(int[] nums, int k) {
+        int count = 0;
+        int start = 0, end = 0, temp = 1;
+
+        while (end < nums.length) {
+            temp *= nums[end];
+            while (end >= start && temp >= k) {
+                temp /= nums[start++];
+            }
+            count += end-start+1;
+            end++;
+        }
+        return count;
+    }
+
+    /*
+            Scenario:   10  5  2  6
+                  end = ^ (10)         --> 0-0+1 = 1 [10]
+                  end =     ^ (50)     --> 1-0+1 = 2 [5],[10,5]
+                  end =        ^ (100) --> 2-1+1 = 2 [2],[5,2]
+                start = ^ (100/10)     --> 1
+                  end =           ^    --> 3-1+1 = 3 [6],[2,6],[5,2,6]
+     */
+}
